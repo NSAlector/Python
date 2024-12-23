88e1df1e9dcc70875f03a885986b398a17e3c049
NSAlector <103880217+NSAlector@users.noreply.github.com>
2024-12-23 13:15:21 +0300

    Add files via upload

---
+++ Lesson_4_module/PY_practice_4_module_process/first_module/print.py
@@ -0,0 +1,6 @@
+
+def printing():
+    print("Hello from the first module")
+
+if __name__ == '__main__':
+    print("Hello, it's the first module")
---
+++ Lesson_4_module/PY_practice_4_module_process/module_example.py
@@ -0,0 +1,7 @@
+import first_module.print as fp
+import second_module.print
+
+if __name__ == '__main__':
+    fp.printing()
+    second_module.print.printing()
+
